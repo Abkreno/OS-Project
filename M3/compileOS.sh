079a581857cfe80d87ced900717d5ce2b8af86fa
kady <elkadyabdelrahman@gmail.com>
2015-04-15 17:27:17 +0200

    Modifying compileOs script for shell compilation

--- M3/compileOS.sh
+++ M3/compileOS.sh
@@ -1,6 +1,12 @@
 bcc -ansi -c -o kernel.o kernel.c
 as86 kernel.asm -o kernel_asm.o
 ld86 -o kernel -d kernel.o kernel_asm.o
+
+# Shell
+bcc -ansi -c -o shell.o shell.c
+as86 lib.asm -o lib.o
+ld86 -o shell -d shell.o lib.o
+
 gcc -o loadFile loadFile.c
 dd if=/dev/zero of=floppya.img bs=512 count=2880
 dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
@@ -10,4 +16,4 @@ dd if=dir.img of=floppya.img bs=512 count=1 seek=2 conv=notrunc
 dd if=message.txt of=floppya.img bs=512 count=1 seek=30 conv=notrunc
 #./loadFile message.txt
 #./loadFile tstprg
-./loadFile tstpr2
+# ./loadFile tstpr2
