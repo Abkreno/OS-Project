f6c05d3e6624cffd758730469b8b6ae2319d5264
Abkreno <mohamedelhamamsy789@gmail.com>
2015-04-15 18:11:52 +0200

    adds function equals to check if 2 strings are equal

--- M3/shell.c
+++ M3/shell.c
@@ -1,3 +1,5 @@
+int equals(char*,char*);
+
 main( void ) {
 
   while(1) {
@@ -17,4 +19,16 @@ main( void ) {
 
   }
 
+}
+
+int equals(char* str1 , char* str2)
+{
+	int i = 0;
+	while(1)
+	{
+		if(str1[i] != str2[i])
+			return 0;
+		if(str1[i++] == '\0')
+			return 1;
+	}
 }
