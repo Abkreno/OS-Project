375dd38b7d74202bf2b46937623b5fee38dad608
kady <elkadyabdelrahman@gmail.com>
2015-04-27 13:21:46 +0200

    Compiling loadFile.c

--- M4/compileOS.sh
+++ M4/compileOS.sh
@@ -7,6 +7,10 @@ bcc -ansi -c -o kernel.o kernel.c
 as86 kernel.asm -o kernel_asm.o
 ld86 -o kernel -d kernel.o kernel_asm.o
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
+
+# loadFile
+gcc -o loadFile loadFile.c
+
 bcc -ansi -c -o shell.o shell.c
 as86 lib.asm -o lib.o
 ld86 -o shell -d shell.o lib.o
