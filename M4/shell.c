e640f1131ad8960b9450cb079b797469faffa6b3
Abkreno <mohamedelhamamsy789@gmail.com>
2015-04-29 16:15:32 +0300

    Adds func copyMessage to stdout to shell.c

--- M4/shell.c
+++ M4/shell.c
@@ -1,8 +1,10 @@
 int equals(char*,char*);
 void clearBuffer(char* buffer, int length);
+void copyMessageToStdout(char*);
 char command[81];
 char argPtr1[81];
 char argPtr2[81];
+char stdout[81];
 char file[13312];
 
 main( void ) {
@@ -42,7 +44,8 @@ main( void ) {
       interrupt(0x21,4,argPtr1,0x2000,0); // executeProgram
     } else if(equals(command,"delete\0")) {
       //TODO Print a message if not file found
-      interrupt(0x21,7,argPtr1,0,0); // deleteFile
+      copyMessageToStdout("Hellloooo");
+      interrupt(0x21,7,argPtr1,0,stdout); // deleteFile
     } else if(equals(command,"copy\0")){ //copy File
       //TODO Print a message if file not found
       interrupt(0x21,3,argPtr1,file,0); //reading the file in buffer
@@ -65,6 +68,13 @@ void clearBuffer(char* buffer, int length){
   for(i=0;i<length;i++){
     buffer[i] = 0x00;
   }
+}
+
+void copyMessageToStdout(char* message){
+  int i;
+  for (i = 0; message[i] != '\0'; i++){
+    stdout[i] = message[i];
+  };
 }
 
 int equals(char* str1 , char* str2)
